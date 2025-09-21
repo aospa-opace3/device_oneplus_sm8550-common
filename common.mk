d9fea98d6b839a0485975e4646c9112d778e30ed
Bruno Martins <bgcngm@gmail.com>
2025-09-21 23:09:30 +0100

    sm8550-common: Decommonize UDFPS related parts
    Not all OnePlus SM8550-based devices have UDFPS support.
    
    Change-Id: I9881cd5a70d163475eb47af34add4527bca47f17

--- common.mk
+++ common.mk
@@ -144,8 +144,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
-$(call soong_config_set,surfaceflinger,udfps_lib,//hardware/oplus:libudfps_extension.oplus)
-
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:$(TARGET_COPY_OUT_ODM)/etc/gps.conf
@@ -297,8 +295,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.multihal \
-    sensors.dynamic_sensor_hal \
-    sensors.oplus
+    sensors.dynamic_sensor_hal
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
