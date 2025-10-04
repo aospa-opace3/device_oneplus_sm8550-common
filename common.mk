1057388d2bdc20b2bf1dd75070c1628ddedfc199
AdarshGrewal <adarshgrewal@gmail.com>
2025-10-04 15:38:33 +0800

    sm8550-common: Switch to common qti av and media components
    @uwu-gl: Currently aospa donot have vendor media components for 5.15, so
    keep them in tree.
    
    Change-Id: Ib017223e45734ba275695cbce49d5ad8eb9e7220
    Signed-off-by: uwu-gl <moegluwu0@gmail.com>

--- common.mk
+++ common.mk
@@ -218,15 +218,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
-# Media
-PRODUCT_COPY_FILES += \
-    $(AUDIO_HAL_DIR)/configs/common/codec2/media_codecs_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2_audio.xml \
-    $(AUDIO_HAL_DIR)/configs/common/codec2/service/1.0/c2audio.vendor.base-arm64.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/c2audio.vendor.base-arm64.policy \
-    $(AUDIO_HAL_DIR)/configs/common/codec2/service/1.0/c2audio.vendor.ext-arm64.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/c2audio.vendor.ext-arm64.policy
-
-PRODUCT_PACKAGES += \
-    libpalclient
-
 # Memtrack
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service
@@ -299,6 +290,8 @@ PRODUCT_PACKAGES += \
 
 # QTI common components
 TARGET_COMMON_QTI_COMPONENTS := \
+    av \
+    media \
     perf \
     telephony \
     wfd \
