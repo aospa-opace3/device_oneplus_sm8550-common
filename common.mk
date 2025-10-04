9a2029771340511e9a5343a3cb7cc31905aad276
uwu-gl <moegluwu0@gmail.com>
2025-10-04 15:00:36 +0800

    sm8550-common: Switch to common qti wfd component
    Change-Id: Ic0e1d3d2453b6a3172014db5ba859d62d641b736
    Signed-off-by: uwu-gl <moegluwu0@gmail.com>

--- common.mk
+++ common.mk
@@ -129,7 +129,8 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm-service.clearkey \
+    android.hardware.drm@1.3.vendor
 
 # Enforce generic ramdisk allow list
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
@@ -278,7 +279,8 @@ PRODUCT_PACKAGES += \
 
 # QTI common components
 TARGET_COMMON_QTI_COMPONENTS := \
-    perf
+    perf \
+    wfd
 
 # Sensors
 PRODUCT_PACKAGES += \
