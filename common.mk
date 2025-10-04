2fadfc7e8f6c9c7b5a21c903ebd944e599121fd2
uwu-gl <moegluwu0@gmail.com>
2025-10-04 21:45:03 +0800

    sm8550-common: Switch to common qti gps component
    Change-Id: Ia439bd3cb917747c1e118332a2976dfd8e3c1b0d
    Signed-off-by: uwu-gl <moegluwu0@gmail.com>

--- common.mk
+++ common.mk
@@ -140,9 +140,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_ODM)/etc/gps.conf
 
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
-
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health-service.qti \
@@ -253,6 +250,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     adreno \
     av \
     display \
+    gps \
     media \
     perf \
     telephony \
