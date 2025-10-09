4c83f04a38a9ca4202e7dcbcc5a8bc27bcb7780d
Fazil Sheik <fazilsheik@aospa.co>
2025-10-09 23:18:37 +0800

    sm8550-common: Set as nosdcard
    Change-Id: I94656fef4eb2bde35ff799d167cf2f9b27ad4021
    Signed-off-by: uwu-gl <moegluwu0@gmail.com>

--- common.mk
+++ common.mk
@@ -52,6 +52,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Characteristics
+PRODUCT_CHARACTERISTICS := nosdcard
+
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
