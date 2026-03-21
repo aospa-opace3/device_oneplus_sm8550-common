bc756e6c3f25f57090a8e8b16eabb24c48b57d39
uwu-gl <moegluwu0@gmail.com>
2026-03-21 19:00:35 +0800

    fixup! sm8550-common: AOSPA bringup

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -161,6 +161,7 @@ BOOT_SECURITY_PATCH := 2025-10-01
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # SEPolicy
+include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/oplus/sepolicy/qti/SEPolicy.mk
 
 # UFS
--- extract-files.py
+++ extract-files.py
@@ -19,6 +19,7 @@ from extract_utils.main import (
 
 namespace_imports = [
     'device/oneplus/sm8550-common',
+    'hardware/qcom-caf/sm8550',
     'hardware/qcom/wlan',
     'hardware/oplus',
     'vendor/qcom/common/vendor/adreno-t',
